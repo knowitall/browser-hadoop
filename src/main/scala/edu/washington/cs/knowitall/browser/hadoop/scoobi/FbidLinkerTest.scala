7818c0c23f104beb0877c3b1e4f42480ac7898bd
Rob <rbart@cs.washington.edu>
2012-04-17 19:49:18 -0700

    wrote wordcount code in fbidtester, added jar plugin to make it the mainclass

--- src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/FbidLinkerTest.scala
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/FbidLinkerTest.scala
@@ -1,9 +1,19 @@
 package edu.washington.cs.knowitall.browser.hadoop.scoobi
 
-import com.nicta.scoobi.Scoobi._
+  import com.nicta.scoobi.Scoobi._
+  import com.nicta.scoobi.DList._
+  import com.nicta.scoobi.io.text.TextInput._
+  import com.nicta.scoobi.io.text.TextOutput._
 
-class FbidLinkerTest extends ScoobiApp {
+class FbidLinkerTest {
 
+  def main(allArgs: Array[String]) = withHadoopArgs(allArgs) { args =>
   
+    val lines = fromTextFile(args(0))
+  
+    val counts = lines.flatMap(_.split(" ")).map(word => (word, 1)).groupByKey.combine[String, Int](_+_)
+
+    persist(toTextFile(counts, args(1)))
+  }
   
 }
