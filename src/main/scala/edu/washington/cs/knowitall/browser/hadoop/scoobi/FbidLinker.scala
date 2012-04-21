04b303e2c19266ed8a1da562131c30b02144f52b
Rob <rbart@cs.washington.edu>
2012-04-20 21:51:51 -0700

    fixed a bug in the return type of the reducer. Added some timing info once every 10Kth line or so, and now only attempts linking on args with NNPs.

--- src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/FbidLinker.scala
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/FbidLinker.scala
@@ -1,35 +1,35 @@
 package edu.washington.cs.knowitall.browser.hadoop.scoobi
 
-  import com.nicta.scoobi.Scoobi._
-  import com.nicta.scoobi.DList._
-  import com.nicta.scoobi.DList
-  import com.nicta.scoobi.io.text.TextInput._
-  import com.nicta.scoobi.io.text.TextInput
-  import com.nicta.scoobi.io.text.TextOutput._
-  import com.nicta.scoobi.io.text.TextOutput
-  
-  import java.io.File
-  import java.io.FileWriter
-  
-  import scala.collection.JavaConversions._
-  
-  import edu.washington.cs.knowitall.browser.extraction.ReVerbExtraction
-  import edu.washington.cs.knowitall.browser.extraction.ExtractionGroup
-  import edu.washington.cs.knowitall.browser.extraction.ReVerbExtractionGroup
-  import edu.washington.cs.knowitall.browser.hadoop.entity.TopCandidatesFinder
-  import edu.washington.cs.knowitall.browser.hadoop.entity.EntityLinker
-  import edu.washington.cs.knowitall.browser.hadoop.entity.Pair
-  
-  import edu.washington.cs.knowitall.nlp.extraction.ChunkedExtraction
-  
-  
-  
+import com.nicta.scoobi.Scoobi._
+import com.nicta.scoobi.DList._
+import com.nicta.scoobi.DList
+import com.nicta.scoobi.io.text.TextInput._
+import com.nicta.scoobi.io.text.TextInput
+import com.nicta.scoobi.io.text.TextOutput._
+import com.nicta.scoobi.io.text.TextOutput
+
+import java.io.File
+import java.io.FileWriter
+
+import scala.util.Random
+import scala.collection.JavaConversions._
+
+import edu.washington.cs.knowitall.common.Timing._
+import edu.washington.cs.knowitall.browser.extraction.ReVerbExtraction
+import edu.washington.cs.knowitall.browser.extraction.ExtractionGroup
+import edu.washington.cs.knowitall.browser.extraction.ReVerbExtractionGroup
+import edu.washington.cs.knowitall.browser.hadoop.entity.TopCandidatesFinder
+import edu.washington.cs.knowitall.browser.hadoop.entity.EntityLinker
+import edu.washington.cs.knowitall.browser.hadoop.entity.Pair
+
+import edu.washington.cs.knowitall.nlp.extraction.ChunkedExtraction
+
 class FbidLinker(val stemmer: TaggedStemmer) {
 
   case class RVTuple(arg1: String, rel: String, arg2: String) {
     override def toString = "%s, %s, %s".format(arg1, rel, arg2)
   }
-  
+
   // returns an (arg1, rel, arg2) tuple of normalized string tokens
   def getNormalizedKey(extr: ReVerbExtraction): RVTuple = {
     def pairs(arg: ChunkedExtraction) = arg.getTokens.zip(arg.getPosTags)
@@ -38,78 +38,81 @@ class FbidLinker(val stemmer: TaggedStemmer) {
     val arg2Norm = stemmer.stemAll(pairs(extr.source.getArgument2)).mkString(" ")
     RVTuple(arg1Norm, relNorm, arg2Norm)
   }
-  
+
   def getKeyValuePair(line: String): Option[(String, String)] = {
     // parse the line to a ReVerbExtraction
-      val extrOpt = ReVerbExtraction.fromTabDelimited(line.split("\t"))._1
-      
-      extrOpt match {
-        case Some(extr) => Some((getNormalizedKey(extr).toString, line))
-        case None => None
-      }
+    val extrOpt = ReVerbExtraction.fromTabDelimited(line.split("\t"))._1
+
+    extrOpt match {
+      case Some(extr) => Some((getNormalizedKey(extr).toString, line))
+      case None => None
+    }
   }
-  
+
   def getEntity(el: EntityLinker, arg: String, head: ReVerbExtraction, sources: Seq[String]): Option[(String, String)] = {
-    
-    
-    
+
+       
     var argEntity: Option[(String, String)] = None
-    
+
     val tryEL = el.getBestFbidFromSources(arg, sources)
     if (tryEL != null) argEntity = Some((tryEL.one, tryEL.two))
     argEntity
   }
-  
+
   def processGroup(el: EntityLinker, key: String, rawExtrs: Iterable[String]): Option[ExtractionGroup[ReVerbExtraction]] = {
-    
+
     def failure(msg: String = "") = {
-      System.err.println("Error processing in processGroup: "+msg+", key: "+key);
+      System.err.println("Error processing in processGroup: " + msg + ", key: " + key);
       rawExtrs.foreach(str => System.err.println(str))
       None
     }
-    
+
     val extrs = rawExtrs.flatMap(line => ReVerbExtraction.fromTabDelimited(line.split("\t"))._1)
-    
+
     val head = extrs.head
-    
+
     val origTuple = RVTuple(head.a1t, head.rt, head.a2t)
     val normTuple = getNormalizedKey(head)
-    
-    if (!normTuple.toString.equals(key)) return failure("Key Mismatch: "+normTuple.toString+" != "+key)
-    
+
+    if (!normTuple.toString.equals(key)) return failure("Key Mismatch: " + normTuple.toString + " != " + key)
+
     val sources = extrs.map(e => e.source.getSentence().getTokensAsString()).toSeq
+
+    val arg1Entity = if (head.source.getArgument1().getPosTags().exists(_.startsWith("NNP"))) {
+      getEntity(el, head.arg1Tokens, head, sources).map(_._1)
+    } else {
+      None
+    }
     
-    val arg1Entity = getEntity(el, head.arg1Tokens, head, sources).map(_._1)
-    val arg2Entity = getEntity(el, head.arg2Tokens, head, sources).map(_._1)
-    
+    val arg2Entity = if (head.source.getArgument2().getPosTags().exists(_.startsWith("NNP"))) {
+      getEntity(el, head.arg2Tokens, head, sources).map(_._1)
+    } else {
+      None
+    }
+
     val instances = extrs.map((_, "TeST", None))
-    
+
     val newGroup = new ExtractionGroup(
-    		normTuple.arg1,
-    		normTuple.rel,
-    		normTuple.arg2,
-    		arg1Entity,
-    		arg2Entity,
-    		None,
-    		None,
-    		instances
-    )
-    
-   Some(newGroup)
+      normTuple.arg1,
+      normTuple.rel,
+      normTuple.arg2,
+      arg1Entity,
+      arg2Entity,
+      None,
+      None,
+      instances)
+
+    Some(newGroup)
   }
-  
-  
-  
-    
-  
+
 }
 
 object FbidLinker {
-    
-    def main(args: Array[String]) = withHadoopArgs(args) { a =>
-    
+
+  def main(args: Array[String]) = withHadoopArgs(args) { a =>
+
     val flink = new FbidLinker(TaggedStemmer.getInstance)
-      
+
     val (inputPath, outputPath) = (a(0), a(1))
 
     val lines: DList[String] = TextInput.fromTextFile(inputPath)
@@ -117,15 +120,22 @@ object FbidLinker {
     // first we need to define what will be our key. We strip only articles - everything else just gets normalized... 
     // normalized by something that takes POS tags, so it performs best.
     val keyValuePair: DList[(String, String)] = lines.flatMap { line => flink.getKeyValuePair(line) }
-    
-    val groups = keyValuePair.groupByKey.flatMap{ case (key, sources) => 
+
+    val groups = keyValuePair.groupByKey.flatMap {
+      case (key, sources) =>
         val el = new EntityLinker
-    	flink.processGroup(el, key, sources) match {
-    	  case Some(group) => ReVerbExtractionGroup.toTabDelimited(group)
-    	  case None => None
-    	}
+        val (t, result) = time {
+          flink.processGroup(el, key, sources) match {
+            case Some(group) => Some(ReVerbExtractionGroup.toTabDelimited(group))
+            case None => None
+          }
+        }
+
+        if (Random.nextDouble < 0.00001) System.err.println("Group processing time: %s ms".format(Milliseconds.format(t)))
+
+        result
     }
-    
+
     DList.persist(TextOutput.toTextFile(groups, outputPath + "/test-results"));
   }
 }
