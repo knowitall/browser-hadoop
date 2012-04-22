ccaac3975cc1e6951cf3b080b59d3bbc0ae7a5df
Rob <rbart@cs.washington.edu>
2012-04-21 20:43:07 -0700

    reverted

--- src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiEntityLinker.scala
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiEntityLinker.scala
@@ -38,31 +38,7 @@ class ScoobiEntityLinker(val stemmer: TaggedStemmer) {
     override def toString = "%s__%s__%s".format(arg1, rel, arg2)
   }
   
-  // returns an (arg1, rel, arg2) tuple of normalized string tokens
-  def getNormalizedKey(extr: ReVerbExtraction): RVTuple = {
-    def pairs(arg: ChunkedExtraction) = arg.getTokens.toSeq.zip(arg.getPosTags.toSeq)
-    
-    val arg1Pairs = pairs(extr.source.getArgument1).filter(!_._2.equals("DT"))
-    val relPairs = pairs(extr.source.getRelation).filter(!_._2.equals("DT"))
-    val arg2Pairs = pairs(extr.source.getArgument2).filter(!_._2.equals("DT"))
-    
-    val arg1Norm = stemmer.stemAll(arg1Pairs)
-    val relNorm = stemmer.stemAll(relPairs)
-    val arg2Norm = stemmer.stemAll(arg2Pairs)
-    
-    
-    RVTuple(arg1Norm.mkString(" ").toLowerCase, relNorm.mkString(" ").toLowerCase, arg2Norm.mkString(" ").toLowerCase)
-  }
 
-  def getKeyValuePair(line: String): Option[(String, String)] = {
-    // parse the line to a ReVerbExtraction
-    val extrOpt = ReVerbExtraction.fromTabDelimited(line.split("\t"))._1
-
-    extrOpt match {
-      case Some(extr) => Some((getNormalizedKey(extr).toString, line))
-      case None => None
-    }
-  }
 
   def getEntity(el: EntityLinker, arg: String, head: ReVerbExtraction, sources: Seq[String]): Option[ArgEntity] = {
 
@@ -74,22 +50,11 @@ class ScoobiEntityLinker(val stemmer: TaggedStemmer) {
     argEntity
   }
 
-  def processGroup(el: EntityLinker, key: String, rawExtrs: Iterable[String]): Option[ExtractionGroup[ReVerbExtraction]] = {
+  def linkEntities(el: EntityLinker, group: ExtractionGroup[ReVerbExtraction]): ExtractionGroup[ReVerbExtraction] = {
 
-    def failure(msg: String = "") = {
-      System.err.println("Error processing in processGroup: " + msg + ", key: " + key);
-      rawExtrs.foreach(str => System.err.println(str))
-      None
-    }
-
-    val extrs = rawExtrs.flatMap(line => ReVerbExtraction.fromTabDelimited(line.split("\t"))._1)
+    val extrs = group.instances.map(_._1)
 
     val head = extrs.head
-
-    val origTuple = RVTuple(head.a1t, head.rt, head.a2t)
-    val normTuple = getNormalizedKey(head)
-
-    if (!normTuple.toString.equals(key)) return failure("Key Mismatch: " + normTuple.toString + " != " + key)
 
     val sources = extrs.map(e => e.source.getSentence().getTokensAsString()).toSeq
 
@@ -105,19 +70,17 @@ class ScoobiEntityLinker(val stemmer: TaggedStemmer) {
       None
     }
 
-    val instances = extrs.map((_, "TeST", None))
-
     val newGroup = new ExtractionGroup(
-      normTuple.arg1,
-      normTuple.rel,
-      normTuple.arg2,
+      group.arg1Norm,
+      group.relNorm,
+      group.arg2Norm,
       arg1Entity,
       arg2Entity,
-      Seq.empty[ArgEntity],
-      Seq.empty[ArgEntity],
-      instances.toSet[(ReVerbExtraction, String, Option[Double])])
+      group.arg1Types,
+      group.arg2Types,
+      group.instances)
 
-    Some(newGroup)
+    newGroup
   }
 
 }
@@ -134,29 +97,15 @@ object ScoobiEntityLinker {
 
     val lines: DList[String] = TextInput.fromTextFile(inputPath)
 
-    // first we need to define what will be our key. We strip only articles - everything else just gets normalized... 
-    // normalized by something that takes POS tags, so it performs best.
-    val keyValuePair: DList[(String, String)] = lines.flatMap { line => flink.getKeyValuePair(line) }
-
-    val groups = keyValuePair.groupByKey.flatMap {
-      case (key, sources) =>
-        //val el = new EntityLinker
-        val el = linkerCache.getOrElseUpdate(Thread.currentThread(), new EntityLinker())
-        val (t, result) = time {
-          flink.processGroup(el, key, sources) match {
-            case Some(group) => Some(ReVerbExtractionGroup.toTabDelimited(group))
-            case None => None
-          }
-        }
-
-        if (Random.nextDouble < 0.0002) {
-          System.err.println("Group processing time: %s for key %s, group:".format(Milliseconds.format(t), key))
-          System.err.println(if (result.isDefined) result.get else "None")
-        }
-
-        result
+    val linkedGroups: DList[String] = lines.flatMap { line => 
+      val el = linkerCache.getOrElseUpdate(Thread.currentThread(), new EntityLinker())
+    	val extrOp = ReVerbExtractionGroup.fromTabDelimited(line.split("\t"))._1
+    	extrOp match {
+        case Some(extr) => Some(ReVerbExtractionGroup.toTabDelimited(flink.linkEntities(el, extr)))
+        case None => None
+      }
     }
 
-    DList.persist(TextOutput.toTextFile(groups, outputPath + "/"));
+    DList.persist(TextOutput.toTextFile(linkedGroups, outputPath + "/"));
   }
 }
