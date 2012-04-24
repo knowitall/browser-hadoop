e13b10b34f929f0ed52ae25439b26244b55b63da
Rob <rbart@cs.washington.edu>
2012-04-24 14:34:11 -0700

    added a Scoobi job to do grouping and linking together in the reducer.

---
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiReVerbGrouperLinker.scala
@@ -0,0 +1,45 @@
+package edu.washington.cs.knowitall.browser.hadoop.scoobi
+
+import com.nicta.scoobi.Scoobi._
+import com.nicta.scoobi.DList._
+import com.nicta.scoobi.DList
+import com.nicta.scoobi.io.text.TextInput._
+import com.nicta.scoobi.io.text.TextInput
+import com.nicta.scoobi.io.text.TextOutput._
+import com.nicta.scoobi.io.text.TextOutput
+
+import java.io.File
+import java.io.FileWriter
+
+import scala.util.Random
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+
+import edu.washington.cs.knowitall.common.Timing._
+import edu.washington.cs.knowitall.browser.extraction.ReVerbExtraction
+import edu.washington.cs.knowitall.browser.extraction.ArgEntity
+import edu.washington.cs.knowitall.browser.extraction.ExtractionGroup
+import edu.washington.cs.knowitall.browser.extraction.ReVerbExtractionGroup
+import edu.washington.cs.knowitall.browser.hadoop.entity.TopCandidatesFinder
+import edu.washington.cs.knowitall.browser.hadoop.entity.EntityLinker
+import edu.washington.cs.knowitall.browser.hadoop.entity.Pair
+
+import edu.washington.cs.knowitall.nlp.extraction.ChunkedExtraction
+
+object ScoobiReVerbGrouperLinker {
+  
+  def main(args: Array[String]) = withHadoopArgs(args) { a =>
+
+    val (inputPath, outputPath) = (a(0), a(1))
+
+    // serialized ReVerbExtractions
+    val extrs: DList[String] = TextInput.fromTextFile(inputPath)
+    
+    // serialized ExtractionGroup[ReVerbExtraction]
+    val groups = ScoobiReVerbGrouper.groupExtractions(extrs)
+    
+    val linkedGroups = ScoobiEntityLinker.linkGroups(groups)
+    
+    DList.persist(TextOutput.toTextFile(linkedGroups, outputPath + "/"));
+  }
+}
