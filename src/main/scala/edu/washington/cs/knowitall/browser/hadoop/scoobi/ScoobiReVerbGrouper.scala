ac8ee2c28c8dbdb1bdca0caed13a83cdb2bf2043
Rob <rbart@cs.washington.edu>
2012-04-26 14:30:14 -0700

    added better reporting to reverbgrouper

--- src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiReVerbGrouper.scala
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiReVerbGrouper.scala
@@ -37,31 +37,32 @@ class ScoobiReVerbGrouper(val stemmer: TaggedStemmer, val corpus: String) {
   var extrsProcessed = 0
   var groupsProcessed = 0
   
+  var largestGroup = 0
+
   case class RVTuple(arg1: String, rel: String, arg2: String) {
     override def toString = "%s__%s__%s".format(arg1, rel, arg2)
   }
-  
+
   // returns an (arg1, rel, arg2) tuple of normalized string tokens
   def getNormalizedKey(extr: ReVerbExtraction): RVTuple = {
     def pairs(arg: ChunkedExtraction) = arg.getTokens.toSeq.zip(arg.getPosTags.toSeq)
-    
+
     val arg1Pairs = pairs(extr.source.getArgument1).filter(!_._2.equals("DT"))
     val relPairs = pairs(extr.source.getRelation).filter(!_._2.equals("DT"))
     val arg2Pairs = pairs(extr.source.getArgument2).filter(!_._2.equals("DT"))
-    
+
     val arg1Norm = stemmer.stemAll(arg1Pairs)
     val relNorm = stemmer.stemAll(relPairs)
     val arg2Norm = stemmer.stemAll(arg2Pairs)
-    
-    
+
     RVTuple(arg1Norm.mkString(" ").toLowerCase, relNorm.mkString(" ").toLowerCase, arg2Norm.mkString(" ").toLowerCase)
   }
 
   def getKeyValuePair(line: String): Option[(String, String)] = {
-    
+
     extrsProcessed += 1
     if (extrsProcessed % 20000 == 0) System.err.println("Extractions processed: %d".format(extrsProcessed))
-    
+
     // parse the line to a ReVerbExtraction
     val extrOpt = ReVerbExtraction.fromTabDelimited(line.split("\t"))._1
 
@@ -72,18 +73,18 @@ class ScoobiReVerbGrouper(val stemmer: TaggedStemmer, val corpus: String) {
   }
 
   def processGroup(key: String, rawExtrs: Iterable[String]): Option[ExtractionGroup[ReVerbExtraction]] = {
-    
+
     groupsProcessed += 1
-    if (groupsProcessed % 10000 == 0) System.err.println("Groups processed: %d".format(groupsProcessed))
+    if (groupsProcessed % 10000 == 0) System.err.println("Groups processed: %d, current key: %s, largest group: %d".format(groupsProcessed, key, largestGroup))
 
     def failure(msg: String = "") = {
-      System.err.println("Error processing in processGroup: " + msg + ", key: " + key);
+      System.err.println("Error in processGroup: " + msg + ", key: " + key);
       rawExtrs.foreach(str => System.err.println(str))
       None
     }
 
     val extrs = rawExtrs.flatMap(line => ReVerbExtraction.fromTabDelimited(line.split("\t"))._1)
-    		
+
     val head = extrs.head
 
     val origTuple = RVTuple(head.a1t, head.rt, head.a2t)
@@ -94,11 +95,13 @@ class ScoobiReVerbGrouper(val stemmer: TaggedStemmer, val corpus: String) {
     val sources = extrs.map(e => e.source.getSentence().getTokensAsString()).toSeq
 
     val arg1Entity = None
-    
+
     val arg2Entity = None
 
-    val instances = extrs.map((_, corpus, None))
+    val instances = extrs.map((_, corpus, None)).toSet[(ReVerbExtraction, String, Option[Double])]
 
+    if (instances.size > largestGroup) largestGroup = instances.size
+    
     val newGroup = new ExtractionGroup(
       normTuple.arg1,
       normTuple.rel,
@@ -107,7 +110,7 @@ class ScoobiReVerbGrouper(val stemmer: TaggedStemmer, val corpus: String) {
       arg2Entity,
       Seq.empty[ArgEntity],
       Seq.empty[ArgEntity],
-      instances.toSet[(ReVerbExtraction, String, Option[Double])])
+      instances)
 
     Some(newGroup)
   }
@@ -115,30 +118,34 @@ class ScoobiReVerbGrouper(val stemmer: TaggedStemmer, val corpus: String) {
 }
 
 object ScoobiReVerbGrouper {
-  
+
+  var calls = 0L
   val grouperCache = new mutable.HashMap[Thread, ScoobiReVerbGrouper]
-  
+
   /** extrs --> grouped by normalization key */
-  def groupExtractions(extrs: DList[String], corpus:String): DList[String] = {
-    
-    val keyValuePair: DList[(String, String)] = extrs.flatMap { line => 
+  def groupExtractions(extrs: DList[String], corpus: String): DList[String] = {
+
+    val keyValuePair: DList[(String, String)] = extrs.flatMap { line =>
+      calls += 1
+      if (calls % 10000 == 0) System.err.println("Grouper Cache size: " + grouperCache.size)
       val grouper = grouperCache.getOrElseUpdate(Thread.currentThread, new ScoobiReVerbGrouper(TaggedStemmer.getInstance, corpus))
-      grouper.getKeyValuePair(line) 
-      }
-    
+      grouper.getKeyValuePair(line)
+    }
+
     keyValuePair.groupByKey.flatMap {
       case (key, sources) =>
-         val grouper = grouperCache.getOrElseUpdate(Thread.currentThread, new ScoobiReVerbGrouper(TaggedStemmer.getInstance, corpus))
-        //val el = new EntityLinker
+        calls += 1
+        if (calls % 10000 == 0) System.err.println("Grouper Cache size: " + grouperCache.size)
+        val grouper = grouperCache.getOrElseUpdate(Thread.currentThread, new ScoobiReVerbGrouper(TaggedStemmer.getInstance, corpus))
         grouper.processGroup(key, sources) match {
-          
-            case Some(group) => Some(ReVerbExtractionGroup.toTabDelimited(group))
-            case None => None
-          }
+
+          case Some(group) => Some(ReVerbExtractionGroup.toTabDelimited(group))
+          case None => None
+        }
     }
-    
+
   }
-  
+
   def main(args: Array[String]) = withHadoopArgs(args) { a =>
 
     val (inputPath, outputPath, corpus) = (a(0), a(1), a(2))
