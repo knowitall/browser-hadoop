151d6bc32399cc8baa5b2dd4ab34af2e2aec61bb
Rob Bart <rbart@cs.washington.edu>
2012-09-08 21:28:27 -0700

    modifying relation counter to include a sample of most common arg1/2 strings

--- src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/RelationCounter.scala
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/RelationCounter.scala
@@ -6,6 +6,8 @@ import util.ExtractionSentenceRecord
 import edu.washington.cs.knowitall.tool.stem.MorphaStemmer
 import edu.washington.cs.knowitall.tool.postag.PostaggedToken
 
+import scala.collection.mutable
+
 /**
  * Produces a list of id, relation string, frequency in descending order of frequency.
  */
@@ -14,8 +16,24 @@ object RelationCounter extends ScoobiApp {
   private val stemmerLocal = new ThreadLocal[MorphaStemmer] { override def initialValue = new MorphaStemmer }
   def stemmer = stemmerLocal.get
   
-  case class CountedRelation(val rel: String, val postags: String, val freq: Int) {
-    override def toString = Seq(freq, rel, postags).mkString("\t")
+  case class Relation(val tokens: Seq[PostaggedToken]) {
+    override def toString = tokens.map { token => "%s_%s".format(token.string, token.postag) }.mkString(" ")
+    def auxString: String = Seq(tokens.map(_.string).mkString(" "), tokens.map(_.postag).mkString(" ")).mkString("\t")
+  }
+  object Relation {
+    def fromString(relString: String): Option[Relation] = {
+      try {
+        val tokens = relString.split(" ").map { relSplit =>
+          relSplit.split("_") match {
+            case Array(string, postag) => new PostaggedToken(postag, string, 0)
+            case _ => return None
+          }
+        }
+        Some(Relation(tokens))
+      } catch {
+        case e: Exception => { e.printStackTrace; None }
+      }
+    }
   }
   
   def run(): Unit = {
@@ -39,34 +57,61 @@ object RelationCounter extends ScoobiApp {
     // serialized ReVerbExtractions
     val input: DList[String] = fromTextFile(inputPath)
     
-    val relations = input flatMap toRelationString
+    val relations = input flatMap toRelationArgs
     
-    val relationsGrouped = relations.groupByKey
+    val grouped = relations.groupByKey
 
-    val relationsCounted = relationsGrouped.map { case (rel, postags) => 
-      val postag = postags.head
-      CountedRelation(rel, postag, postags.size) 
-    } filter { _.freq >= minFrequency} map { _.toString }
+    val tabulated = grouped map tabulateGroup
     
-    persist(toTextFile(relationsCounted, outputPath + "/"))
+    persist(toTextFile(tabulated, outputPath + "/"))
   }
   
   val badTokens = Set("a", "an", "the")
   
   def filterTokens(token: PostaggedToken) = !badTokens.contains(token.string)
   
-  def toRelationString(inputRecord: String): Option[(String, String)] = {
+  def filterArgString(argString: String): Boolean = {
+    argString.length >= 2 && argString.length < 30
+  }
+  
+  // returns (Relation.doubleString, most frequent arg1s, most frequent arg2s)
+  def tabulateGroup(group: (String, Iterable[(String, String)])): String = group match { case (relString, argStrings) =>
+    val arg1Counts = new mutable.HashMap[String, MutableInt]
+    val arg2Counts = new mutable.HashMap[String, MutableInt]
+    
+    val rel = Relation.fromString(relString).get
+    var size = 0
+    
+    argStrings.iterator.foreach { case (arg1, arg2) =>
+      size += 1
+      val sizeOk = size < 200000
+      if (sizeOk && filterArgString(arg1)) arg1Counts.getOrElseUpdate(arg1, MutableInt(0)).inc
+      if (sizeOk && filterArgString(arg2)) arg2Counts.getOrElseUpdate(arg2, MutableInt(0)).inc
+    }
+    
+    def mostFrequent(counts: mutable.Map[String, MutableInt]): Seq[String] = {
+      counts.iterator.filter({ case (string, mutFreq) => mutFreq.value > 1 }).toSeq.sortBy(-_._2.value).map(_._1).take(6)
+    }
+    
+    Seq(size, rel.auxString, mostFrequent(arg1Counts).mkString(", "), mostFrequent(arg2Counts).mkString(", ")).mkString("\t")
+  }
+  
+  // (Relation.toString, (arg1String, arg2String))
+  def toRelationArgs(inputRecord: String): Option[(String, (String, String))] = {
     try { 
       val esr = new ExtractionSentenceRecord(inputRecord)
       val relTokens = esr.norm1Rel.split(" ").map(_.toLowerCase)
       val relPos = esr.norm1RelPosTags.split(" ")
       val posTokens = relTokens.zip(relPos) map { case (tok, pos) => new PostaggedToken(pos, tok, 0) } filter filterTokens
-      val stemmed = posTokens map stemmer.stemToken
-      val result = stemmed.map(tok => (tok.lemma, tok.token.postag))
-      val rel = result.map(_._1).mkString(" ")
-      val postags = result.map(_._2).mkString(" ")
-      if (result.isEmpty) None else Some((rel, postags))
+      if (posTokens.isEmpty) None else Some((Relation(posTokens).toString, (esr.arg1, esr.arg2)))
     } 
     catch { case e: Exception => { e.printStackTrace; None }}
   }
+  
+  // Utility Hacks for counting a long list of strings
+  
+  case class MutableInt(var value: Int) {
+    def inc: Unit = { value += 1 }
+  }
 }
+
