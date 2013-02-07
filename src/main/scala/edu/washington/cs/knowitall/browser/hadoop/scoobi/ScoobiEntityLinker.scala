972d1999e806dbf8d2f1c85cb15ef4382a4d3314
Justin Huang <jstn@cs.washington.edu>
2013-02-06 19:30:15 -0800

    Changed entity linker to link the head phrase of the args instead of the args themselves.

--- src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiEntityLinker.scala
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiEntityLinker.scala
@@ -88,7 +88,7 @@ class ScoobiEntityLinker(val subLinkers: Seq[EntityLinker], val stemmer: TaggedS
     val (arg1Entity, arg1Types) = if (reuseLinks && group.arg1.entity.isDefined) {
       (group.arg1.entity, group.arg1.types)
     } else {
-      val entity = getEntity(randomLinker, head.arg1Text, head, sources) match {
+      val entity = getEntity(randomLinker, head.arg1Head, head, sources) match {
         case Some(rawEntity) => { arg1sLinked += 1; entityConversion(rawEntity) }
         case None => (Option.empty[FreeBaseEntity], Set.empty[FreeBaseType])
       }
@@ -99,7 +99,7 @@ class ScoobiEntityLinker(val subLinkers: Seq[EntityLinker], val stemmer: TaggedS
     val (arg2Entity, arg2Types) = if (reuseLinks && group.arg2.entity.isDefined) {
       (group.arg2.entity, group.arg2.types)
     } else {
-      val entity = getEntity(randomLinker, head.arg2Text, head, sources) match {
+      val entity = getEntity(randomLinker, head.arg2Head, head, sources) match {
         case Some(rawEntity) => { arg2sLinked += 1; entityConversion(rawEntity) }
         case None => (Option.empty[FreeBaseEntity], Set.empty[FreeBaseType])
       }
