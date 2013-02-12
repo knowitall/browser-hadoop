a4a1c08451bf8a84666843458c33248bf5a82fe3
Justin Huang <jstn@cs.washington.edu>
2013-02-12 15:39:48 -0800

    Added head phrase comparison job.

--- src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiEntityLinker.scala
+++ src/main/scala/edu/washington/cs/knowitall/browser/hadoop/scoobi/ScoobiEntityLinker.scala
@@ -153,8 +153,8 @@ object ScoobiEntityLinker extends ScoobiApp {
     new ScoobiEntityLinker(el, TaggedStemmer.instance)
   }
 
-  def linkGroups(groups: DList[String], minFreq: Int, maxFreq: Int, reportInterval: Int, skipLinking: Boolean): DList[String] = {
-
+  def linkGroups(groups: DList[String], minFreq: Int, maxFreq: Int, reportInterval: Int,
+      skipLinking: Boolean): DList[String] = {
     if (skipLinking) return frequencyFilter(groups, minFreq, maxFreq, reportInterval, skipLinking)
 
     groups.flatMap { line =>
